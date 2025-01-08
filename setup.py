0d9857371b3f0733534e89a0b4395ee76df7904f
Eme <marianela.encina@outlook.es>
2025-01-08 08:59:30 -0300

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miPrimerRelease",                                 # Nombre del paquete
+    version="0.1.0",                                        # Versión inicial
+    packages=find_packages(),                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",          # Breve descripción
+    author="Marianela",                                     # Tu nombre
+    author_email="marianela.encina@outlook.es",             # Tu correo electrónico
+    url="https://github.com/maetenco/miPrimerRelease",      # URL del proyecto
+)
